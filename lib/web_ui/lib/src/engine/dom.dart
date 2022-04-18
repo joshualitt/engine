a66a255b94aae96a320280e6b4a1c4ed9f78062b
Joshua Litt <joshualitt@google.com>
2022-04-18 17:38:32 +0000

    [web]

--- lib/web_ui/lib/src/engine/dom.dart
+++ lib/web_ui/lib/src/engine/dom.dart
@@ -11,6 +11,7 @@ class DomWindow {}
 
 extension DomWindowExtension on DomWindow {
   external DomDocument get document;
+  external DomNavigator get navigator;
 }
 
 @JS('window')
@@ -18,10 +19,22 @@ external DomWindow get domWindow;
 
 @JS()
 @staticInterop
+class DomNavigator {}
+
+extension DomNavigatorExtension on DomNavigator {
+  external int? get maxTouchPoints;
+  external String get vendor;
+  external String? get platform;
+  external String get userAgent;
+}
+
+@JS()
+@staticInterop
 class DomDocument {}
 
 extension DomDocumentExtension on DomDocument {
   external /* List<Node> */ List<Object?> querySelectorAll(String selectors);
+  external DomHTMLElement createElement(String name, [dynamic options]);
 }
 
 @JS()
@@ -42,9 +55,38 @@ class DomHTMLMetaElement {}
 
 extension DomHTMLMetaElementExtension on DomHTMLMetaElement {
   external String get name;
-  external String get content;
-
   external set name(String value);
+  external String get content;
+}
+
+@JS()
+@staticInterop
+class DomCanvasElement extends DomHTMLElement {
+  factory DomCanvasElement({int? width, int? height}) {
+    final DomCanvasElement canvas =
+        domWindow.document.createElement('canvas') as DomCanvasElement;
+    if (width != null) {
+      canvas.width = width;
+    }
+    if (height != null) {
+      canvas.height = height;
+    }
+    return canvas;
+  }
+}
+
+extension DomCanvasElementExtension on DomCanvasElement {
+  external int? get width;
+  external set width(int? value);
+  external int? get height;
+  external set height(int? value);
+
+  Object? getContext(String contextType, [Map<dynamic, dynamic>? attributes]) {
+    return js_util.callMethod(this, 'getContext', <Object?>[
+      contextType,
+      if (attributes != null) js_util.jsify(attributes)
+    ]);
+  }
 }
 
 Object? domGetConstructor(String constructorName) =>
